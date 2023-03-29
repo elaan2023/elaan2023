b260ace7a60692093382dea113fa7ef994b1b6c2
elaan2023 <129280877+elaan2023@users.noreply.github.com>
2023-03-29 16:16:41 +0530

    Added initial files

---
+++ js/typing.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#info", {
+    // Waits 1000ms after typing "First"
+    strings: ["Quest of Unknown", "Singing Sensation Anadi Mishra LIVE IN CONCERT On 15 APRIL 2023"],
+    loop: true,
+    loopCount: Infinity,
+});
